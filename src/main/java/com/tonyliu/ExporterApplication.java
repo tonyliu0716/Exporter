55530c7a65b1f891ac54b014262a21680fe0e9a0
tonyliu0716 <tonyliu0716@gmail.com>
2017-08-03 17:35:54 -0500

    Connect with mysql database with SSH

--- src/main/java/com/tonyliu/ExporterApplication.java
+++ src/main/java/com/tonyliu/ExporterApplication.java
@@ -3,11 +3,14 @@ package com.tonyliu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+
 @SpringBootApplication
 public class ExporterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExporterApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/tonyliu/controller/TesterController.java
+++ src/main/java/com/tonyliu/controller/TesterController.java
@@ -1,7 +1,14 @@
 package com.tonyliu.controller;
 
+import com.tonyliu.entity.Course;
+import com.tonyliu.entity.Xblock;
+import com.tonyliu.service.XblockService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by tao on 8/3/17.
@@ -10,9 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TesterController {
 
+    @Autowired
+    private XblockService service;
+
     @RequestMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @RequestMapping("/getXblocks")
+    public List<Xblock> getXblocks() {
+        System.out.println("getting data from OpenEdx database.");
+        return service.getXblocks();
+    }
+
+    @RequestMapping(value = "/getCourseId", method = RequestMethod.GET)
+    public List<Course> getAllCourseId() {
+        return service.getCourses();
     }
 
 }
